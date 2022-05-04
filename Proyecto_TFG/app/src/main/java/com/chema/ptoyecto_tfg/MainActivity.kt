4a90f8bc6da9fd876af771608ca999b93633bcbf
Chema <chemaruiz94@gmail.com>
2022-05-04 19:37:25 +0200

    Commit - Project

---
+++ Proyecto_TFG/app/src/main/java/com/chema/ptoyecto_tfg/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.chema.ptoyecto_tfg
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
