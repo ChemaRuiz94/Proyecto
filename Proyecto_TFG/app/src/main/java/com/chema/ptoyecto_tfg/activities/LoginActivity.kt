d292f4b184d1181938db07fb6f6ef110d3c590c7
Chema <chemaruiz94@gmail.com>
2022-05-04 20:49:38 +0200

    Commit - Login First Design

---
+++ Proyecto_TFG/app/src/main/java/com/chema/ptoyecto_tfg/activities/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.chema.ptoyecto_tfg.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.chema.ptoyecto_tfg.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
