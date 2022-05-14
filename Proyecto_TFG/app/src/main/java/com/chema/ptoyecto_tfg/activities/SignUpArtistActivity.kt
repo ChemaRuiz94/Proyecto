865a742bae61ed563e05c82d7801d850273a9d8a
Chema <chemaruiz94@gmail.com>
2022-05-14 15:39:15 +0200

    Commit - SignUp Design

---
+++ Proyecto_TFG/app/src/main/java/com/chema/ptoyecto_tfg/activities/SignUpArtistActivity.kt
@@ -0,0 +1,12 @@
+package com.chema.ptoyecto_tfg.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.chema.ptoyecto_tfg.R
+
+class SignUpArtistActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up_artist)
+    }
+}
