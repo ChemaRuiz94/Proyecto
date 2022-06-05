7ce324617ac0db7d490a4a116026c49dd7e7e874
Chema <chemaruiz94@gmail.com>
2022-06-05 20:23:30 +0200

    Commit - Search (First Design)

--- Proyecto_TFG/app/src/main/java/com/chema/ptoyecto_tfg/navigation/basic/BasicUserNavDrawActivity.kt
+++ Proyecto_TFG/app/src/main/java/com/chema/ptoyecto_tfg/navigation/basic/BasicUserNavDrawActivity.kt
@@ -44,7 +44,7 @@ class BasicUserNavDrawActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_basic_profile, R.id.nav_slideshow
+                R.id.nav_search, R.id.nav_basic_profile, R.id.nav_slideshow
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
